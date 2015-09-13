85a7c5d04f450155c52526352f0d429648078de2
Ryan Westby <jrwestby13@gmail.com>
2015-09-12 18:18:22 -0700

    Wrote Title Screen and Game Over Screen

--- src/djinn/TextHandler.java
+++ src/djinn/TextHandler.java
@@ -1,36 +1,72 @@
 package djinn;
 
 import java.awt.Font;
+import java.io.InputStream;
+
 import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.Color;
+import org.newdawn.slick.util.ResourceLoader;
 
 public class TextHandler {
 	
-	TrueTypeFont font;
-	TrueTypeFont font2;
+	TrueTypeFont titleFontSm;
+	TrueTypeFont titleFontLg;
+	
+	TrueTypeFont endFontSm;
+	TrueTypeFont endFontLg;
+	
+	private boolean antiAlias = false;
+
 	
 	public TextHandler(Djinn djinn) {
 	}
 	
 	public void titleText(Djinn djinn) {
 		Color.white.bind();
+
+		// load font from file
+		try {
+			InputStream inputStream	= ResourceLoader.getResourceAsStream("joystik.ttf");
+	
+			Font awtFont2 = Font.createFont(Font.TRUETYPE_FONT, inputStream);
+			awtFont2 = awtFont2.deriveFont(24f); // set font size
+			titleFontSm = new TrueTypeFont(awtFont2, antiAlias);
+			
+			awtFont2 = awtFont2.deriveFont(30f);
+			titleFontLg = new TrueTypeFont(awtFont2, antiAlias);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
-		this.font = new TrueTypeFont(new Font("Futura", Font.BOLD, 30), true);
-		this.font2 = new TrueTypeFont(new Font("Futura", Font.BOLD, 48), true);
-				
-		font.drawString(djinn.displayWidth/2-200, djinn.displayHeight/2 - 400, "Welcome to...", Color.black);
-		font2.drawString(djinn.displayWidth/2-100, djinn.displayHeight/2 - 360, "Djinn", Color.black);
+		titleFontSm.drawString(100, 200, "Welcome to...", Color.black);
+		titleFontLg.drawString(160, 300, "DJINN", Color.black);
 		
-		font.drawString(djinn.displayWidth/2-200, djinn.displayHeight/2 - 200	, "Please Press Enter", Color.black);
+		titleFontSm.drawString(80, 400	, "Select Your Style:", Color.black);
+		titleFontSm.drawString(160, 450	, "Defender", Color.black);
+		titleFontSm.drawString(160, 500	, "Tetron", Color.black);
+		titleFontSm.drawString(160, 550	, "Djinn", Color.black);
 	}
 	
 	public void endText(Djinn djinn) {
 		Color.white.bind();
 		
-		this.font = new TrueTypeFont(new Font("Futura", Font.BOLD, 30), true);
-		this.font2 = new TrueTypeFont(new Font("Futura", Font.BOLD, 48), true);
-				
-		font2.drawString(djinn.displayWidth/2-100, djinn.displayHeight/2 - 360, "GAME OVER", Color.black);
-		font.drawString(djinn.displayWidth/2-200, djinn.displayHeight/2 - 400, "Try Again? Press Enter", Color.black);
+		try {
+			InputStream inputStream	= ResourceLoader.getResourceAsStream("joystik.ttf");
+	
+			Font awtFont2 = Font.createFont(Font.TRUETYPE_FONT, inputStream);
+			awtFont2 = awtFont2.deriveFont(24f); // set font size
+			endFontSm = new TrueTypeFont(awtFont2, antiAlias);
+			
+			awtFont2 = awtFont2.deriveFont(30f);
+			endFontLg = new TrueTypeFont(awtFont2, antiAlias);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		endFontLg.drawString(130, 300, "GAME OVER", Color.black);
+		
+		endFontSm.drawString(100, 400, "Select Your Fate:", Color.black);
+		endFontSm.drawString(200, 450, "Another Try", Color.black);
+		endFontSm.drawString(200, 500, "End It", Color.black);
 	}
 }
